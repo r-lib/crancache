effbc72db23e8d6eb8b41ddbe11ea68fbbdf0e3a
Gábor Csárdi <csardi.gabor@gmail.com>
2017-06-27 21:10:04 +0100

    Revert metadata caching for now, will branch

--- R/download-packages.R
+++ R/download-packages.R
@@ -27,23 +27,17 @@ download_packages <- function(
 
   update_cache <- should_update_crancache()
 
-  ## Do not specify contrib.url here, it does not have our repos
-  available <- update_metadata_for_install(method, type, repos)
-
-  withr::with_options(
-    list(repos = if (is.null(repos)) getOption("repos") else myrepos),
-    tryCatch(
-      utils::download.packages(
-        pkgs = pkgs,
-        destdir = destdir,
-        available = available,
-        repos = myrepos,
-        ## We don't specify contriburl, on purpose
-        method = method,
-        type = type,
-        ...),
-      error = function(e) stop(e),
-      finally = if(update_cache) update_cache(destdir)
-    )
+  tryCatch(
+    utils::download.packages(
+      pkgs = pkgs,
+      destdir = destdir,
+      available = NULL,                 # overwritten
+      repos = myrepos,
+      ## We don't specify contriburl, on purpose
+      method = method,
+      type = type,
+      ...),
+    error = function(e) stop(e),
+    finally = if(update_cache) update_cache(destdir)
   )
 }
--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,30 @@
 
 `%||%` <- function(l, r) if (is.null(l)) r else l
 
-`%:::%` <- function(pkg, name) getFromNamespace(name, pkg)
+url_scheme <- function(url) {
+  if (grepl("^[^:]+:", url)) {
+    sub("(^[^:]+):.*", "\\1", url)
+  } else {
+    ""
+  }
+}
+
+bak_file <- function(path) {
+  paste0(path, ".bak")
+}
 
 make_key <- function(x) {
   gsub("[^a-z0-9]+", "-", tolower(x))
+}
+
+read_dcf <- function(file) {
+  as.list(read.dcf(file)[1,])
+}
+
+write_dcf <- function(x, file) {
+  m <- matrix(unlist(x), nrow = 1)
+  colnames(m) <- names(x)
+  write.dcf(m, file = file)
 }
 
 try_silently <- function(expr) {
@@ -28,16 +48,4 @@ pkg_name_from_file <- function(x) {
 
 isFALSE <- function(x) {
   identical(x, FALSE)
-}
-
-ungzip <- function(path) {
-  if (! grepl("\\.gz$", path)) stop("Not the a gzipped file")
-  target <- sub("\\.gz$", "", path)
-  gzf <- gzfile(path, open = "r")
-  on.exit(close(gzf))
-  writeLines(readLines(gzf), target)
-}
-
-r_version <- function() {
-  paste0(R.Version()[c("major", "minor")], collapse = ".")
 }
