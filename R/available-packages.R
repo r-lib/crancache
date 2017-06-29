fb73d9f2ab939e61c1ff562dcbb1e49ad2963139
Gábor Csárdi <csardi.gabor@gmail.com>
2017-06-29 12:32:20 +0100

    Cache filtered package list

--- R/available-packages.R
+++ R/available-packages.R
@@ -6,6 +6,7 @@
 #'
 #' @inheritParams utils::available.packages
 #' @export
+#' @importFrom digest sha1
 #' @family caching package management functions
 
 available_packages <- function(contriburl = contrib.url(repos, type),
@@ -20,7 +21,78 @@ available_packages <- function(contriburl = contrib.url(repos, type),
                                      repos = repos))
   }
 
-  myrepos <- if (is.null(repos)) {
+  update_repo_metadata(contriburl)
+
+  call <- match.call()
+  call[[1]] <- quote(utils::available.packages)
+  call$repos <-
+    if (is.null(repos)) NULL else c(get_crancache_repos(), repos)
+  call$filters <- list()
+
+  res <- eval(call, envir = parent.frame())
+
+  filters <- get_filters(filters)
+
+  hash <- list(sha1(res), sha1(filters))
+  if (identical(hash, data_env$available_hash)) {
+    data_env$available
+  } else {
+    res <- apply_filters(res, filters)
+    data_env$available <- res
+    data_env$available_hash <- hash
+    res
+  }
+}
+
+apply_filters <- function(pkgs, filters) {
+  if (!length(pkgs)) return(res)
+
+  for (f in filters) {
+    if (!length(pkgs)) break
+    if (is.character(f)) {
+      f <- ("utils" %:::% "available_packages_filters_db")[[f]]
+    }
+    if (!is.function(f)) stop("invalid 'filters' argument, not a function")
+    pkgs <- f(pkgs)
+  }
+
+  pkgs
+}
+
+get_filters <- function(filters) {
+  filters <- filters %||%
+    getOption("available_packages_filters") %||%
+    "utils" %:::% "available_packages_filters_default"
+
+  if (is.list(filters) && isTRUE(filters$add)) {
+    filters$add <- NULL
+    filters <- c("utils" %:::% "available_packages_filters_default", filters)
+  }
+  filters
+}
+
+## This will update / inject the RDS metadata cache that
+## download.packages will pick up automatically
+## Note that for 'both' we cannot supply 'available', but we
+## inject the cached RDS files, nevertheless
+
+update_metadata_for_install <- function(method, type, repos) {
+  if (type %in% c("source", "both")) {
+    av_src <- available_packages(method = method, fields = NULL,
+                                 type = "source", filters = NULL,
+                                 repos = repos)
+  }
+  if (type %in% c("binary", "both") ||
+      (.Platform$pkgType != "source" && type == .Platform$pkgType)) {
+    av_bin <- available_packages(method = method, fields = NULL,
+                                 type = "binary", filters = NULL,
+                                 repos = repos)
+  }
+
+  ## If type = "both", then available must be NULL
+  if (type == "source") {
+    av_src
+  } else if (type == "both") {
     NULL
   } else {
     c(get_crancache_repos(), repos)
